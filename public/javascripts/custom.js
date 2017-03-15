4225964263c4c8b127b7d60186bfba5702d150d8
isurunix <isurunix@gmail.com>
2017-03-16 00:57:15 +0530

    UI modifications

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -12,5 +12,22 @@ $(document).ready(function () {
    toCombo.enableFilteringMode(true);
    toCombo.selectOption(0);
 
-   calendar = new dhtmlXCalendarObject("calendar");
+   // calendar = new dhtmlXCalendarObject("calendar");
+   $("#calendar").pickadate({
+      format: 'yyyy-mm-dd',
+      formatSubmit: 'yyyy-mm-dd'
+   });
+
 });
+
+function searchTrain() {
+    var fromStationCode = $('#from-station').val();
+    var toStationCode = $('#to-station').val();
+    var date = $('#calender').val();
+    var url = "http://railway.lankagate.gov.lk/train/searchTrain?startStationID="+fromStationCode+"&endStationID="+toStationCode+"&searchDate="+date+
+        "&startTime=00:00:00&endTime=23:59:00&lang=en"
+
+    $.get(url, function (data) {
+       $('#table').val(data);
+    });
+}
